2474eb9bda6f21f0748f9a4102084cfec7e68b80
tdewin <tdewin@users.noreply.github.com>
2023-01-24 10:28:44 +0100

    Update fourlines.full.js

--- src/fourlines.full.js
+++ src/fourlines.full.js
@@ -30,9 +30,8 @@ function arrayXToY(start,stop,step) {
 }
 
 //rm
-//make an error on purpose to check workflow
 tests.push(function test_arrayXToY(inject) {
-	return equalCheck(arrayXToY(0,20,3),([0, 3, 6, 9, 12, 15, 19]));
+	return equalCheck(arrayXToY(0,20,3),([0, 3, 6, 9, 12, 15, 18]));
 })
 //srm
 
